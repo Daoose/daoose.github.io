db4cb64efe674600623432c001a73dd1adc92837
Oleg <oleg.yevik@gmail.com>
2016-09-03 20:22:06 +1200

    new pages

--- js/main.js
+++ js/main.js
@@ -1,11 +1,46 @@
 $(document).ready(function(){
     //ResizePage();
-    $(".content#one").click(function() {
-        $('html, body').animate({scrollTop: $(".content#two").offset().top}, 1000);
+    // $(".content#one").click(function() {
+    //     $('html, body').animate({scrollTop: $(".content#two").offset().top}, 1000);
+    // });
+    // $(".page1box#one").click(function() {
+    //     $('html, body').animate({scrollTop: $(".content#three").offset().top}, 1000);
+    // }); 
+    var $dom = $('body');
+    var $frame = $("iframe");
+    var scroll = this.scrollTop;      
+
+   
+    $dom.bind( 'mousewheel DOMMouseScroll', function ( e ) {
+        var e0 = e.originalEvent,
+            delta = e0.wheelDelta || -e0.detail;
+        var wh = $(window).height(); 
+        var scroll = this.scrollTop + ( delta < 0 ? 1 : -1 ) * wh;
+        scroll = Math.round(scroll/wh) * wh;
+        $dom.stop().animate({ scrollTop: scroll }, 800);
+        console.log(scroll);
+        e.preventDefault();
+    });
+
+    $(document).keydown(function(e) {
+        // up
+        if (e.which == 38 || e.which == 33) {
+            var wh = $(window).height(); 
+            var scroll = $(window).scrollTop() - wh;
+            console.log(scroll);
+            scroll = Math.round(scroll/wh) * wh;
+            $dom.stop().animate({ scrollTop: scroll }, 800);
+            e.preventDefault();
+        }
+        // down
+        if (e.which == 34 || e.which == 40) {
+            var wh = $(window).height(); 
+            var scroll = $(window).scrollTop() + wh;
+            scroll = Math.round(scroll/wh) * wh;
+            $dom.stop().animate({ scrollTop: scroll }, 800);
+            e.preventDefault();
+        }        
 });
-   $(".page1box#one").click(function() {
-        $('html, body').animate({scrollTop: $(".content#three").offset().top}, 1000);
-}); 
     
 });
 
